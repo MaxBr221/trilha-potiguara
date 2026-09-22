c0acf195ead32449d9968fb9f447451f551afa5c
Maxsuel <maxsuel.lima@dcx.ufpb.br>
2026-09-22 19:48:10 -0300

    fix: corrige ortografia de Mani'oka para Mandi'oka

---
+++ src/main/resources/db/migration/V45__fix_mandioka_spelling.sql
@@ -0,0 +1,4 @@
+-- Correção: Mani'oka -> Mandi'oka
+
+UPDATE exercicios SET resposta_correta = 'Mandi''oka' WHERE resposta_correta = 'Mani''oka';
+UPDATE exercicio_opcoes SET opcao = 'Mandi''oka' WHERE opcao = 'Mani''oka';
